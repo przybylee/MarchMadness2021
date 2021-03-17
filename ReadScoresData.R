b315a6d415df81f95f06721498389ea2d6a7c412
Lee Przybylski <leep@iastate.edu>
2021-03-16 23:02:43 -0500

    Sorted 2021 team names

--- ReadScoresData.R
+++ ReadScoresData.R
@@ -11,9 +11,13 @@ head(scores)
 teams <- read.csv("cbb21_no_results.csv", header = TRUE, sep = ",")
 head(teams)
 
-teams_bartorvik <- as.character(unique(teams$team))
-teams_scores <- as.character(unique(scores$Team))
-char_min <- stringdist::stringdist(team_names[1],team_ids)
+teams_bartorvik <- unique(teams$team)
+teams_scores <- unique(scores$Team)
+char_min <- stringdist::stringdist(teams_bartorvik[1],teams_scores)
+results <- data.frame(cbind(teams_scores, char_min))
+head(results)
+results[order(char_min)[1:5],]
+match(char_min, teams_scores)
 
 #read in team ids from Kaggle data
 MTeams <- read.csv("Kaggle Data/MTeams.csv")
@@ -23,8 +27,20 @@ head(MTeams)
 #Here is a function for identifying the closest match between two vectors of strings
 name_match <- function(x,y){
   char_min <- stringdist::stringdist(x, y)
-  y[which.min(char_min)]
+  indx <- which.min(char_min)
+  y[indx]
 }
+name_search <- function(x,y){
+  y[grepl(x,y)]
+}
+
+#This functions lists the n best matches according to string dist
+name_list <- function(name, list, n = 5){
+  char_min <- stringdist::stringdist(name,list)
+  results <- data.frame(cbind(list, char_min))
+  results[order(char_min)[1:n],]
+}
+name_list("Gonzaga", teams_bartorvik, 11)
 name_match(MTeams$TeamName[1], teams_scores)
 name_match(MTeams$TeamName[1], teams_bartorvik)
 
@@ -38,9 +54,39 @@ for (j in 1:L){
 }
 head(MTeams)
 tail(MTeams)
+#How many exact matches:
+sum(MTeams$TeamName == MTeams$bartorvik)/371
+exact <- MTeams$TeamName == MTeams$bartorvik
+MTeams_keep <- MTeams[exact,]
+MTeams_sort <- MTeams[!exact,]
+#Remove team names that found an exact match
+teams_bartorvik1 <- teams_bartorvik[!(teams_bartorvik %in% MTeams_keep$TeamName)]
+head(teams_bartorvik1)
+head(MTeams_sort)
+#Repeat matching, but only look at bartrovik1 for matches
+L <- length(MTeams_sort$TeamID)
+for (j in 1:L){
+  name <- MTeams_sort$TeamName[j]
+  b_name <- name_match(name, teams_bartorvik1)
+  MTeams_sort$bartorvik[j] <- b_name
+}
+MTeams_sort
+#write.table(MTeams_sort, "bartorvik_sort.csv", col.names = T, row.names = F, sep = ",")
+#Use name search to find best remaining names
+name_search("Michigan", teams_bartorvik1)
+#Read back in sorted teams
+MTeams_sorted <- read.csv("bartorvik_sort.csv", header = T, sep = ",")
+head(MTeams_sorted)
+New <- MTeams_sorted[MTeams_sorted$X == 1, 1:5]
+MTeams <- rbind(MTeams_keep, New)
+Unmatched <- MTeams_sorted[MTeams_sorted$X == 0, 1:5]
+bartorvik2 <- teams_bartorvik1[!(teams_bartorvik1 %in% MTeams$bartorvik)]
+#We save the data from the 42 teams that are unmatched
+
 
 #Create a cloumn in Mteams for the ids in the scores data
 MTeams$scores <- as.character(0)
+L <- length(MTeams$TeamID)
 for (j in 1:L){
   name <- MTeams$TeamName[j]
   s_name <- name_match(name, teams_scores)
@@ -48,6 +94,197 @@ for (j in 1:L){
 }
 head(MTeams)
 tail(MTeams)
+#Remove exact matches from consideration
+exact <- MTeams$TeamName == MTeams$scores
+v <- ifelse(exact, 1,0)
+matched <- MTeams$TeamName[exact]
+teams_scores1 <- teams_scores[!(teams_scores %in% matched)]
+MTeams_sort <- cbind(MTeams, v)
+head(MTeams_sort)
+#write.table(MTeams_sort, "score_teams_sort.csv", col.names = T, row.names = F, sep = ",")
+#Use name_search to confirm exact matches
+name_search("Loyola", teams_scores1)
+name_search("Miami", teams_scores1)
 
-write.table(MTeams, file = "TeamList.csv", col.names = T, row.names = F, sep = ",")
+#Read back in sorted data
+MTeams_sorted <- read.csv("score_teams_sort.csv", header = T, sep = ",")
+head(MTeams_sorted)
+#One row was not matched (Brooklyn)
+MTeams_sorted[MTeams_sorted$v2 == 0,]
+teams_scores2 <- teams_scores1[!(teams_scores1 %in% MTeams_sorted$scores)]
+
+#Save the 3 lists
+write.table(MTeams_sorted, "TeamList0.csv", col.names = T, row.names = F, sep = ",")
+write.table(bartorvik2, "Bartorvik_nomatch.csv", col.names = T, row.names = F, sep = ",")
+write.table(teams_scores2, "scores_nomatch.csv", col.names = T, row.names = F, sep = ",")
+write.table(Unmatched, "Unmatched_ids.csv", col.names = T, row.names = F, sep = ",")
+
+
+#We try to put the unmatched teams in teams_scores2 with a team id
+#MTeams_sorted <- read.csv("TeamList0.csv", header = T, sep = ",")
+#bartorvik2 <- read.csv("TeamList0.csv", header = T, sep = ",")
+#MTeams_sorted <- read.csv("TeamList0.csv", header = T, sep = ",")
+
+#Start with the 58 teams in teams_scores2
+L <- length(teams_scores2)
+v <- as.character(rep(0,L))
+for (j in 1:L){
+  name <- teams_scores2[j]
+  id_name <- name_match(name, MTeams_sorted$TeamName)
+  v[j] <- id_name
+}
+d_names <- data.frame(cbind(teams_scores2, v))
+d_names
+d_names$check <- 0
+d_names[1:5,]
+d_names$check[1:5] <- c(1,0,1,1,1)
+m <- 5
+d_names[(1+m):(5+m),]
+d_names$check[(1+m):(5+m)] <- c(0,1,0,1,1)
+m <- m+5
+d_names[(1+m):(5+m),]
+d_names$check[(1+m):(5+m)] <- c(1,0,1,0,0)
+m <- m+5
+d_names[(1+m):(5+m),]
+d_names$check[(1+m):(5+m)] <- c(1,0,1,0,0)
+m <- m+5
+d_names[(1+m):(5+m),]
+d_names$check[(1+m):(5+m)] <- c(0,0,0,0,1)
+m <- m+5
+d_names[(1+m):(5+m),]
+d_names$check[(1+m):(5+m)] <- c(1,0,1,1,0)
+m <- m+5
+d_names[(1+m):(5+m),]
+d_names$check[(1+m):(5+m)] <- c(0,0,0,1,0)
+m <- m+5
+d_names[(1+m):(5+m),]
+d_names$check[(1+m):(5+m)] <- c(0,1,1,1,1)
+m <- m+5
+d_names[(1+m):(5+m),]
+d_names$check[(1+m):(5+m)] <- c(0,1,1,1,1)
+m <- m+5
+d_names[(1+m):(5+m),]
+d_names$check[(1+m):(5+m)] <- c(0,1,1,0,1)
+m <- m+5
+d_names[(1+m):(5+m),]
+d_names$check[(1+m):(5+m)] <- c(1,1,1,0,1)
+m <- m+5
+d_names[(1+m):(3+m),]
+d_names$check[(1+m):(3+m)] <- c(0,1,1)
+sum(d_names$check)
+# We added 33 names matched
+#rows without matches in teams_scores2
+d_names[d_names$check == 0,]
+list <- MTeams_sorted$TeamName
+lst <- name_list("NorthernIllinois", list )
+print(lst)
+d_names$v[2] <- lst[3,1]
+d_names$check[2] <- 1
+d_names[d_names$check == 0,]
+lst <- name_list("SouthCarolinaState", list, n =5 )
+print(lst)
+list[grepl("Kentucky", list)]
+d_names$v[14] <- lst[3,1]
+d_names$check[14] <- 1
+d_names[d_names$check == 0,]
+lst <- name_list("NorthCarolinaCentral", list, n =10 )
+print(lst)
+list[grepl("Carolina", list)]
+d_names$v[17] <- lst[4,1]
+d_names$check[17] <- 1
+d_names[d_names$check == 0,]
+lst <- name_list("FloridaAM", list, n =5 )
+print(lst)
+list[grepl("Kentucky", list)]
+d_names$v[20] <- lst[2,1]
+d_names$check[20] <- 1
+d_names[d_names$check == 0,]
+lst <- name_list("MississippiValleyState", list, n =5 )
+print(lst)
+list[grepl("Purdue", list)]
+d_names$v[22] <- lst[3,1]
+d_names$check[22] <- 1
+d_names[d_names$check == 0,]
+lst <- name_list("CentralConnecticut", list, n =5 )
+print(lst)
+d_names$v[23] <- lst[2,1]
+d_names$check[23] <- 1
+d_names[d_names$check == 0,]
+lst <- name_list("UMKC", list, n =10 )
+print(lst)
+list[grepl("Missouri", list)]
+d_names$v[27] <- "Missouri KC"
+d_names$check[27] <- -1
+d_names[d_names$check == 0,]
+lst <- name_list("TexasAM", list, n =10 )
+print(lst)
+list[grepl("Corpus", list)]
+d_names$v[31] <- lst[2,1]
+d_names$check[31] <- 1
+d_names[d_names$check == 0,]
+lst <- name_list("AlabamaAM", list, n =10 )
+print(lst)
+list[grepl("Corpus", list)]
+d_names$v[32] <- lst[2,1]
+d_names$check[32] <- 1
+d_names[d_names$check == 0,]
+lst <- name_list("TennesseeMartin", list, n =10 )
+print(lst)
+list[grepl("Corpus", list)]
+d_names$v[35] <- lst[4,1]
+d_names$check[35] <- 1
+d_names[d_names$check == 0,]
+lst <- name_list("CentralArkansas", list, n =10 )
+print(lst)
+list[grepl("Corpus", list)]
+d_names$v[36] <- "Cent Arkansas"
+d_names$check[36] <- -1
+d_names[d_names$check == 0,]
+d_names$v[33] <- "Florida Intl"
+d_names$check[33] <- -1
+d_names[d_names$check == 0,]
+d_names$v[41] <- "TAM C. Christi"
+d_names$check[41] <- -1
+d_names[d_names$check == 0,]
+lst <- name_list("Northwestern", list, n =5 )
+print(lst)
+list[grepl("LA", list)]
+d_names$v[49] <- "Northwestern LA"
+d_names$check[49] <- -1
+d_names[d_names$check == 0,]
+list[grepl("Little Rock", list)]
+d_names$v[56] <- "Ark Little Rock"
+d_names$check[56] <- 1
+
+d_names <- d_names[1:58,]
+d_names$check[10] <-0 
+length(unique(d_names$v[d_names$check == 1]))
+sum(d_names$check == 1)
+#The names in d_names with check = 1 get assigned as a second scores name
+MTeams_sorted$scores2 <- NA
+d_names1 <- d_names[d_names$check ==1,]
+L <- length(d_names1$v)
+for (j in 1:L){
+  team_row <- d_names1$v[[j]]
+  MTeams_sorted[MTeams_sorted$TeamName == team_row, "scores2"] <- d_names1$teams_scores2[j]
+}
+#The names in d_names with check = 1- get matched with the correct row from Unmatched
+head(MTeams_sorted)
+head(Unmatched2)
+d_names2 <- d_names[d_names$check < 0,]
+Unmatched2 <- Unmatched[Unmatched$TeamName %in% d_names2$v,]
+Unmatched2$v <- 0
+Unmatched2$v2 <- 0
+Unmatched2$scores <- NA
+Unmatched2$scores2 <- NA
+L <- length(d_names2$v)
+for (j in 1:L){
+  team_row <- d_names2$v[[j]]
+  Unmatched2[Unmatched2$TeamName == team_row, "scores"] <- d_names2$teams_scores2[j]
+}
+head(MTeams_sorted)
+MTeams_sorted <- rbind(MTeams_sorted, Unmatched2)
+tail(MTeams_sorted)
+
+#write.table(MTeams, file = "TeamList.csv", col.names = T, row.names = F, sep = ",")
 #There are still some serious problems with the matching.
